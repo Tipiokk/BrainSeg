f0d32db93640686decec2954308a8b846c1a33da
Ambistic <nathan_v@hotmail.fr>
2024-11-01 16:44:28 +0100

    Fix unexpected tkinter import error

--- scale_geojson.py
+++ scale_geojson.py
@@ -6,16 +6,14 @@ from shapely.affinity import scale
 import geopandas as gpd
 
 from brainseg.geo import quickfix_multipolygon_qupath
-
-
-def rescale_polygon(polygon, scale_factor):
-    rescaled_polygon = scale(polygon, xfact=scale_factor, yfact=scale_factor, origin=(0, 0))
-    return rescaled_polygon
+from brainseg.polygon import translate_polygon, rescale_polygon
 
 
 def main(args):
     gdf = gpd.read_file(args.geojson)
+    gdf["geometry"] = gdf["geometry"].apply(lambda x: translate_polygon(x, x=-args.center_x, y=-args.center_y))
     gdf["geometry"] = gdf["geometry"].apply(lambda x: rescale_polygon(x, args.scale))
+    gdf["geometry"] = gdf["geometry"].apply(lambda x: translate_polygon(x, x=args.center_x, y=args.center_y))
     geo = geojson.FeatureCollection(
         [geojson.Feature(geometry=row['geometry'].__geo_interface__, properties=row.drop('geometry').to_dict())
          for _, row in gdf.iterrows()]
@@ -33,5 +31,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-g", "--geojson", type=Path)
     parser.add_argument("-s", "--scale", type=float)
+    parser.add_argument("-x", "--center_x", type=float, default=0.0)
+    parser.add_argument("-y", "--center_y", type=float, default=0.0)
     args_ = parser.parse_args()
     main(args_)
